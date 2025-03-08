c66c8dce034c9282396b9cf384e4902e68ddab3b
wit-l <witty.ysl@gmail.com>
2025-03-09 02:00:59 +0800

    Update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,24 +2,14 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const debounce = useCallback((func: Function, delay: number = 300) => {
-    let timerId: number;
-    console.log("in debounce");
-
-    return (...args: any[]) => {
-      clearTimeout(timerId);
-      timerId = setTimeout(func, delay, ...args);
-    };
-  }, []);
-
-  const handleSearch_ = debounce((term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -27,9 +17,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete("query");
     }
-    // console.log(`${pathname}?${params.toString()}`);
     replace(`${pathname}?${params.toString()}`);
-  });
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -39,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleSearch_(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchParams.get("query") || undefined}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
