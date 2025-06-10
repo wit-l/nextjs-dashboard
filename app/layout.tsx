f8387dcceb3de65879336bb415b48087a7619a30
wit-l <witty.ysl@gmail.com>
2025-06-11 01:08:19 +0800

    Replace inter font with lxgw

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import { lxgw } from "@/app/ui/fonts";
 type Props = { children: React.ReactNode };
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${lxgw.className} antialiased`}>{children}</body>
     </html>
   );
 }
